fb6a75c144f3569c2845b4b8a79687a0d672c54f
ap-ch <alejandro.pchirinos@gmail.com>
2023-05-27 15:25:31 +0200

    Arreglar errores

--- implementacion_recambios/api_recambios/main.py
+++ implementacion_recambios/api_recambios/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo.mongo_client import MongoClient
-import yaml
-
+from models.recambio import Recambio
 
 api_recambios = FastAPI()
 mongo_client = MongoClient("mongodb://root:root@recambios_db:27017/")
 taller = mongo_client["taller"]
+
+# CORSMiddleware para método OPTIONS
 
 origins = [
     "127.0.0.1:4000"
@@ -18,37 +19,9 @@ api_recambios.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["POST", "GET", "OPTIONS", "PUT", "DELETE"],
-		allow_headers=["*"],
+    allow_headers=["*"],
     max_age=3600,
 )
-
-@api_recambios.options("/api/v1/recambios")
-async def get_supported_methods_without_param():
-    # Cargar el documento openapi.yaml
-    with open("openapi.yaml", "r") as file:
-        openapi_data = yaml.safe_load(file)
-
-    # Obtener la lista de métodos HTTP soportados del documento
-    methods_data = openapi_data["paths"]["/recambios"]
-
-    # Devolver la lista de métodos HTTP soportados
-    return {
-        "methods_without_param": methods_data
-    }
-
-@api_recambios.options("/api/v1/recambios/{numero_serie}")
-async def get_supported_methods():
-    # Cargar el documento openapi.yaml
-    with open("openapi.yaml", "r") as file:
-        openapi_data = yaml.safe_load(file)
-
-    # Obtener la lista de métodos HTTP soportados del documento
-    methods_data = openapi_data["paths"]["/recambios/{Numero_Serie}"]
-
-    # Devolver la lista de métodos HTTP soportados
-    return {
-        "methods_with_param": methods_data,
-    }
 
 @api_recambios.get("/api/v1/recambios")
 async def lista_recambios(page: int = 0):
@@ -67,57 +40,39 @@ async def lista_recambios(page: int = 0):
 
 @api_recambios.get('/api/v1/recambios/{numero_serie}')
 async def obtener_recambio(numero_serie: str):
-    recambios_cur = taller["recambios"].find({"numero_serie": numero_serie}, {"_id": 0})
-    recambios = {
-        "recambios": [recambio for recambio in recambios_cur],
-    }
-    if not recambios:
+    recambio = taller["recambios"].find_one({"numero_serie": numero_serie}, {"_id": 0})
+    if not recambio:
         raise HTTPException(status_code=404, detail="No se encuentra el recambio")
-    recambioOut = {"recambios": recambios}
+    recambioOut = {"recambios": recambio}
     return recambioOut
 
 @api_recambios.delete('/api/v1/recambios/{numero_serie}')
-async def delete_recambio(numero_serie: str):
-    # Buscar el objeto con el número de serie en la colección
-    recambios_cur = taller["recambios"].find({"numero_serie": numero_serie}, {"_id": 0})
-
-    if recambios_cur:
-        # Eliminar el objeto de la colección
-        taller["recambios"].delete_one({"numero_serie": numero_serie})
+async def borrar_recambio(numero_serie: str):
+    deleted = taller["recambios"].delete_one({"numero_serie": numero_serie}).deleted_count
+    if deleted:
         return {"message": "Recambio eliminado correctamente"}
     else:
-        # No se encontró ningún objeto con el número de serie proporcionado
         raise HTTPException(status_code=404, detail="Recambio no encontrado")
 
 @api_recambios.post('/api/v1/recambios')
-async def add_recambio(recambio: dict):
-    # Comprobar si falta el atributo obligatorio "numero_serie"
-    if "numero_serie" not in recambio:
-        raise HTTPException(status_code=422, detail="Falta el atributo obligatorio 'numero_serie'")
-
-    numero_serie = recambio["numero_serie"]
-
-    # Comprobar si el número de serie ya existe en la colección
-    recambios_cur = taller["recambios"].find({"numero_serie": numero_serie}, {"_id": 0})
-    if recambios_cur:
-        raise HTTPException(status_code=400, detail="El número de serie especificado ya existe")
-
-    # Insertar el nuevo objeto en la colección
-    taller["recambios"].insert_one(recambio)
-    return {"message": "Recambio añadido correctamente"}
+async def crear_recambio(recambio: Recambio):
+    try:
+        taller["recambios"].insert_one(recambio.dict())
+        return {"message": "Recambio añadido correctamente"}
+    except:
+        raise HTTPException(status_code=400, detail="No se pudo añadir el recambio")
 
 @api_recambios.put('/api/v1/recambios/{numero_serie}')
-async def update_recambio(numero_serie: str, recambio_data: dict):
-    # Comprobar si falta el atributo obligatorio "numero_serie" en el body
-    if "numero_serie" not in recambio_data:
-        raise HTTPException(status_code=422, detail="Falta el atributo obligatorio 'numero_serie'")
+async def actualizar_recambio(numero_serie: str, recambio: Recambio):
+    if numero_serie != recambio.numero_serie:
+        raise HTTPException(status_code=400, detail="El número de serie no coincide")
+    
+    found_recambio = taller["recambios"].find_one({"numero_serie": numero_serie}, {"_id": 0})
+    if not found_recambio:
+        raise HTTPException(status_code=404, detail="No se encuentra el recambio")
 
-    # Verificar si el número de serie existe en la colección
-    recambios_cur = taller["recambios"].find({"numero_serie": numero_serie}, {"_id": 0})
-    if not recambios_cur:
-        raise HTTPException(status_code=404, detail="Recambio no encontrado")
-
-    # Actualizar el objeto en la colección
-    taller["recambios"].update_one({"numero_serie": numero_serie}, {"$set": recambio_data})
-
-    return {"message": "Recambio actualizado correctamente"}
+    try:
+        taller["recambios"].update_one({"numero_serie": numero_serie}, {"$set": recambio.dict()})
+        return {"message": "Recambio actualizado correctamente"}    
+    except:
+        raise HTTPException(status_code=400, detail="No se pudo actualizar el recambio")
